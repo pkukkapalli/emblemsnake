d6bdf70dc1f3178fa3a11f4525b113371af417a3
Pradyoth Kukkapalli <pkukkapalli@gmail.com>
2020-09-28 17:17:55 -0400

    Add GA tracking script

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,5 @@ module.exports = {
     'import/named': 'off',
     'import/no-unresolved': 'off',
   },
-  ignorePatterns: ['drawserver/*.js', 'scripts/*.js'],
+  ignorePatterns: ['drawserver/*.js', 'scripts/*.js', 'index.html'],
 };
